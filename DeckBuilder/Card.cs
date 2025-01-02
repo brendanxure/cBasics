d709bb5ce60ed940b9c10fb7ad2cd6b1daaa30ee
brendanobilo <brendanxure@outlook.com>
2025-01-02 20:39:10 +0100

    update

---
+++ DeckBuilder/Card.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckBuilder
+{
+    /// <summary>
+    /// This class defines the properties of a card, 
+    /// specifically its suit and rank. We will also override the ToString() method
+    /// to display the card's information.
+    /// </summary>
+    internal class Card
+    {
+        private string suit;
+        private string rank;
+        public Card(string suit, string rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+
+        public string Suit 
+        { 
+            get => suit; 
+            set {
+                    suit = value;
+                } 
+        }
+        public string Rank 
+        {   
+            get => rank; 
+            set { 
+                    rank = value;
+                } 
+        }
+
+        // Override ToString to display the card information.
+        public override string ToString()
+        {
+            return $"{Rank} of {Suit}";
+        }
+    }
+}

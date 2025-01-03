b2816832f7e26884a5552d3cf745478483a3830d
brendanobilo <brendanxure@outlook.com>
2025-01-03 18:56:14 -0500

    update

---
+++ DeckBuilder/CustomDeck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckBuilder
+{
+    internal class CustomDeck : Deck
+    {
+        // Allows adding custom cards to the deck.
+        public void AddCustomCard(Card card)
+        {
+            Cards.Add(card);
+        }
+
+        public override void InitializeDeck()
+        {
+            // Custom initialization logic can be added here.
+            // For example, an empty deck or a deck with specific custom cards.
+        }
+    }
+}
